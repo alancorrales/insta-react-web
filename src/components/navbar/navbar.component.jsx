244e81f38d60ac0242c2b39b363219e68d60d00a
Alan <a.corrales@sidi.org.br>
2020-08-13 01:09:06 -0300

    Adds dropdown menu for navigating to profile or logging out

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
 import './navbar.styles.scss';
 
-const Navbar = ({ user }) => (
-	<nav className='navbar'>
-		<div className='navbar-content'>
-			<NavLink className='brand-link' to='/'>
-				<h1 className='brand-text'>InstaReact</h1>
-			</NavLink>
-			<img className='profile-picture' src={user.avatar} alt='profile' />
-		</div>
-	</nav>
-);
+import ProfileDropdown from './../profile-dropdown/profile-dropdown.component';
+
+const Navbar = ({ user }) => {
+	const [dropdownHidden, setDropdownHidden] = useState(true);
+
+	return (
+		<nav className='navbar'>
+			<div className='navbar-content'>
+				<NavLink className='brand-link' to='/'>
+					<h1 className='brand-text'>InstaReact</h1>
+				</NavLink>
+				<div className='profile-context'>
+					<img
+						className='profile-picture'
+						src={user.avatar}
+						alt='profile'
+						onClick={() => setDropdownHidden(!dropdownHidden)}
+					/>
+					{dropdownHidden ? null : <ProfileDropdown />}
+				</div>
+			</div>
+		</nav>
+	);
+};
 
 const mapStateToProps = (state) => ({
 	user: state.user,
---
+++ src/components/profile-dropdown/profile-dropdown.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './profile-dropdown.styles.scss';
+import { NavLink } from 'react-router-dom';
+
+const ProfileDropdown = () => (
+	<div className='dropdown'>
+		<NavLink className='dropdown-item' to='/profile'>
+			<span class='material-icons'>person</span>
+			<p className='profile-link'>Profile</p>
+		</NavLink>
+		<NavLink className='dropdown-item' to='/'>
+			<span class='material-icons'>exit_to_app</span>
+			<p className='profile-link'>Log out</p>
+		</NavLink>
+	</div>
+);
+
+export default ProfileDropdown;
